9f2d169feac1f3ecf192dcfc8a9f07cd8bcb43cd
Harish V <harishv207@gmail.com>
2019-04-08 23:36:13 +0800

    Compressed images, deployment script for zeit now

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+    webpack: config => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty'
+        }
 
-    return config
-  }
+        return config
+    },
+    target: 'serverless'
 }
